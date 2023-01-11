fc64f9ff5d6632d81fa6c6b5aab28b4e95b6c522
煎饼果子加十个蛋 <3182703224@qq.com>
2023-01-11 17:14:30 +0800

    axios简化

--- src/api/api-axios.ts
+++ src/api/api-axios.ts
@@ -1,10 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { message as Message } from "antd";
-/**
- * 创建Axios实例
- * 请求域名
- * 最长等待时间
- */
 const request = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 20000,
@@ -12,13 +7,7 @@ const request = axios.create({
     common: { Authorization: `Bearer ${localStorage.getItem("token")}` },
   },
 });
-/**
- * 请求拦截器
- */
 request.interceptors.request.use((config) => config);
-/**
- * 响应拦截器
- */
 request.interceptors.response.use(
   (res) => {
     const { status, statusText, data } = res;
@@ -27,21 +16,11 @@ request.interceptors.response.use(
     Message.warning(statusText);
     return new Promise(() => {});
   },
-  (err) => {
-    const { message } = err;
+  ({ message }) => {
     console.error(message);
     Message.error(message);
     return new Promise(() => {});
   }
 );
-/**
- * 导出实例
- */
-export default async <T = unknown>(params: AxiosRequestConfig) => {
-  return (await request(params)) as unknown as T;
-};
-fetch("/bing", {
-  headers: {
-    responseType: "blob",
-  },
-});
+export default async <T = unknown>(params: AxiosRequestConfig) =>
+  (await request(params)) as unknown as T;
