6aa67454f840daa0e5af59b1edf08ae6fc55c11a
煎饼果子加十个蛋 <3182703224@qq.com>
2023-02-09 23:53:11 +0800

    背景

--- src/page/demo/index.tsx
+++ src/page/demo/index.tsx
@@ -12,6 +12,27 @@ import { RestOutlined } from "@ant-design/icons";
 
 export function PageDemo() {
   const cx = useClass(style);
-  return <div className={cx("box")}></div>;
+  return (
+    <div className={cx("box")}>
+      <div className={cx("glass")}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi,
+        unde nihil? Error, ab. Expedita provident, culpa repellat esse ex a
+        similique ea cumque fuga consequatur aliquam nisi aliquid nam
+        architecto.
+      </div>
+      <div className={cx("grid")}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi,
+        unde nihil? Error, ab. Expedita provident, culpa repellat esse ex a
+        similique ea cumque fuga consequatur aliquam nisi aliquid nam
+        architecto.
+      </div>
+      <div className={cx("shadow")}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi,
+        unde nihil? Error, ab. Expedita provident, culpa repellat esse ex a
+        similique ea cumque fuga consequatur aliquam nisi aliquid nam
+        architecto.
+      </div>
+    </div>
+  );
 }
 export default React.memo(PageDemo);
