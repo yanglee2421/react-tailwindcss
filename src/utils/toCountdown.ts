0c72e9fa71f2461ab0c47a4c9896095f5e8812ef
zhangsheng <yanglee2421@gmail.com>
2023-04-15 22:17:14 +0800

    toCountdown

---
+++ src/utils/toCountdown.ts
@@ -0,0 +1,27 @@
+type Param = ConstructorParameters<typeof Date>[0];
+
+export function toCountdown(param: Param) {
+  const date = new Date(param);
+  console.log(date.toLocaleString());
+  const target = date.getTime() - Date.now();
+
+  const perDay = 1000 * 60 * 60 * 24;
+  const day = Math.floor(target / perDay);
+  const restDay = target % perDay;
+
+  const perHour = 1000 * 60 * 60;
+  const hour = Math.floor(restDay / perHour);
+  const restHour = restDay % perHour;
+
+  const perMin = 1000 * 60;
+  const min = Math.floor(restHour / perMin);
+  const restMin = restDay % perMin;
+
+  const perSec = 1000;
+  const sec = Math.floor(restMin / perSec);
+  const restSec = restMin % perSec;
+
+  const res = `${day}天/${hour}小时/${min}分/${sec}秒/${restSec}毫秒`;
+  console.log(res);
+  return res;
+}
