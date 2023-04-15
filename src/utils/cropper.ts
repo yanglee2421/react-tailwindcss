ac9af50b570550841d2b24c27f9808fd9e57d3ce
zhangsheng <yanglee2421@gmail.com>
2023-04-15 17:30:34 +0800

    sort out code

--- src/utils/cropper.ts
+++ src/utils/cropper.ts
@@ -11,18 +11,18 @@ export function downloadBase64File(dataUrl: string, filename: string) {
   link.download = filename;
   link.click();
   URL.revokeObjectURL(urlBlob);
+}
 
-  function base64Img2Blob(code: string) {
-    const parts = code.split(";base64,");
-    const contentType = parts[0].split(":")[1];
-    const raw = window.atob(parts[1]);
-    const rawLength = raw.length;
-    const uInt8Array = new Uint8Array(rawLength);
-    for (let i = 0; i < rawLength; ++i) {
-      uInt8Array[i] = raw.charCodeAt(i);
-    }
-    return new Blob([uInt8Array], { type: contentType });
+function base64Img2Blob(code: string) {
+  const parts = code.split(";base64,");
+  const contentType = parts[0].split(":")[1];
+  const raw = window.atob(parts[1]);
+  const rawLength = raw.length;
+  const uInt8Array = new Uint8Array(rawLength);
+  for (let i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
   }
+  return new Blob([uInt8Array], { type: contentType });
 }
 
 /**
