a302969c2513215fe4f27ca64f0f2706b06b46e6
zhangsheng <yanglee2421@gmail.com>
2023-04-16 20:32:46 +0800

    Lee氏合名法

--- src/utils/toCountdown.ts
+++ src/utils/toCountdown.ts
@@ -4,15 +4,15 @@ export function toCountdown(...targetTime: TargetTime) {
   const targetDate = new Date(...targetTime);
   const difference = targetDate.getTime() - Date.now();
 
-  const [day, restDay] = getTimeCarry(difference, 1000 * 60 * 60 * 24);
-  const [hour, restHour] = getTimeCarry(restDay, 1000 * 60 * 60);
-  const [min, restMin] = getTimeCarry(restHour, 1000 * 60);
-  const [sec, restSec] = getTimeCarry(restMin, 1000);
+  const [day, restDay] = toTimeCarry(difference, 1000 * 60 * 60 * 24);
+  const [hour, restHour] = toTimeCarry(restDay, 1000 * 60 * 60);
+  const [min, restMin] = toTimeCarry(restHour, 1000 * 60);
+  const [sec, restSec] = toTimeCarry(restMin, 1000);
 
   return `${day}天/${hour}小时/${min}分/${sec}秒/${restSec}毫秒`;
 }
 
-function getTimeCarry(totalTime: number, unit: number) {
+function toTimeCarry(totalTime: number, unit: number) {
   const time = Math.floor(totalTime / unit);
   const restTime = totalTime % unit;
   return [time, restTime];
