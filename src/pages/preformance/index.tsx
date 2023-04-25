8e7faf7857eb46816eec14033b587c837cf3a99f
zhangsheng <yanglee2421@gmail.com>
2023-04-25 14:07:34 +0800

    something

--- src/pages/preformance/index.tsx
+++ src/pages/preformance/index.tsx
@@ -1,7 +1,14 @@
 import style from "./style.module.scss";
-import { useStyle } from "@/hooks";
+import { useStructure, useStyle } from "@/hooks";
 
 export default function MyEdit() {
   const cx = useStyle(style);
+  // 用法
+  const [obj, setObj] = useStructure({
+    name: "孙悟空",
+  });
+  setObj((prev) => {
+    prev.name = "猪八戒";
+  });
   return <></>;
 }
