057921a87200f048d5a63f244ebf16872de296be
路明非 <1060761796@qq.com>
2023-05-07 21:51:12 +0800

    Gallery provider plan

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { RouterProvider } from "react-router-dom";
-import { ConfigProvider, Image, theme } from "antd";
-import { withPortal } from "@/components";
+import { ConfigProvider, theme } from "antd";
+import { GalleryWithPortal } from "@/components";
 import { useDark } from "@/hooks";
 import { useAppDispatch, useAppSelector, theme as slice } from "@/redux";
 import { router } from "@/routes";
-import React, { useMemo } from "react";
 
 export function App() {
-  // 根据 Browser 主题派发 actIsDark
   const dispatch = useAppDispatch();
   useDark(({ matches }) => dispatch(slice.actions.actIsDark(matches)));
-
-  // 根据 store 中的 isDark 返回主题样式
-  const GalleryWithPortal = withPortal(Gallery);
 
   const { darkAlgorithm, defaultAlgorithm } = theme;
   const isDark = useAppSelector((state) => state.theme.isDark);
@@ -24,29 +19,4 @@ export function App() {
       <GalleryWithPortal />
     </ConfigProvider>
   );
-}
-
-function Gallery() {
-  const { galleryIsShow, galleryList } = useAppSelector((state) => state.theme);
-
-  // Image 数组
-  const imgList = useMemo(
-    () => galleryList.map((src) => <Image key={src} src={src} />),
-    [galleryList]
-  );
-
-  // 画廊主体
-  const dispatch = useAppDispatch();
-  const imgGroup = useMemo(() => {
-    const onVisibleChange = (vis: boolean) =>
-      dispatch(slice.actions.actGalleryIsShow(vis));
-
-    return (
-      <Image.PreviewGroup preview={{ visible: galleryIsShow, onVisibleChange }}>
-        {imgList}
-      </Image.PreviewGroup>
-    );
-  }, [galleryIsShow, imgList]);
-
-  return <div className="none">{imgGroup}</div>;
 }
---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+import { useAppDispatch, useAppSelector, theme } from "@/redux";
+import { Image } from "antd";
+import { withPortal } from "@/components";
+
+export function Gallery() {
+  const { galleryIsShow, galleryList } = useAppSelector((state) => state.theme);
+
+  // Image 数组
+  const imgList = useMemo(
+    () => galleryList.map((src) => <Image key={src} src={src} />),
+    [galleryList]
+  );
+
+  // 画廊主体
+  const dispatch = useAppDispatch();
+  const imgGroup = useMemo(() => {
+    const onVisibleChange = (vis: boolean) =>
+      dispatch(theme.actions.actGalleryIsShow(vis));
+
+    return (
+      <Image.PreviewGroup preview={{ visible: galleryIsShow, onVisibleChange }}>
+        {imgList}
+      </Image.PreviewGroup>
+    );
+  }, [galleryIsShow, imgList]);
+
+  return <div className="none">{imgGroup}</div>;
+}
+
+export const GalleryWithPortal = withPortal(Gallery);
