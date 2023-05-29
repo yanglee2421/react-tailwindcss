addf8ff1e5bbbcbd0ed52c214367c65ff3cdda6d
ZhangSheng <yanglee2421@gmail.com>
2023-05-29 18:51:38 +0800

    react-hook-form

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch, login } from "@/redux";
 import style from "./home.module.scss";
 import { useStyle } from "@/hooks";
+import { useForm, Controller } from "react-hook-form";
 
 /**
  * 首页
@@ -14,8 +15,65 @@ export function Component() {
     disPatch(login.actions.actSetState(false));
   };
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { email: "", password: "" },
+    reValidateMode: "onSubmit",
+  });
+
+  const onSubmit = (data: unknown) => {
+    console.log(data);
+  };
+
   return (
     <div className={cx("home b h-100")}>
+      <div>
+        <form
+          action="#"
+          onSubmit={handleSubmit(onSubmit)}
+          onReset={() => reset()}
+          className={cx("home__form")}
+        >
+          <label>Email</label>
+          <input
+            type="text"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "nmsl",
+              },
+            })}
+          />
+          {errors.email && (
+            <p className="text-danger">{errors.email.message}</p>
+          )}
+          <Controller
+            control={control}
+            name="password"
+            rules={{ required: { value: true, message: "wdnmd" } }}
+            render={({ field: { onChange, value }, formState: { errors } }) => {
+              return (
+                <>
+                  <label>
+                    <span>password</span>
+                  </label>
+                  <input type="password" value={value} onChange={onChange} />
+                  {errors.password && (
+                    <p className="text-danger">{errors.password.message}</p>
+                  )}
+                </>
+              );
+            }}
+          ></Controller>
+          <button type="submit">提交</button>
+          <button type="reset">reset</button>
+        </form>
+      </div>
       <button onClick={handleLogout}>log out</button>
     </div>
   );
