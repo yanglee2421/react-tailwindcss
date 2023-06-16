645c26827a1061cfaaad3a139cc7778e9ff97aa3
ZhangSheng <yanglee2421@gmail.com>
2023-06-16 18:59:22 +0800

    use-refresh

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,8 +5,9 @@ import style from "./home.module.scss";
 
 // Hooks Imports
 import { useStyle } from "@/hooks";
-import { useLoginQuery } from "./hooks";
-import { useEffect } from "react";
+// import { useLoginQuery } from "./hooks";
+import { useEffect, useMemo, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export function Component() {
   const cx = useStyle(style);
@@ -16,17 +17,56 @@ export function Component() {
     disPatch(sliceLogin.actions.actSetState(false));
   };
 
-  const { data } = useLoginQuery();
+  // const { data } = useLoginQuery();
 
-  console.log(data);
+  // useEffect(() => {
+  //   if (!data) return;
+  //   localStorage.setItem("token", data.token);
+  // }, [data]);
+
+  const [list, setList] = useState<string[]>([]);
+
+  const listEl = useMemo(() => {
+    return list.map((item) => <li key={item}>{item}</li>);
+  }, [list]);
+
+  const { data } = useQuery({
+    queryKey: ["refresh"],
+    async queryFn() {
+      const time = new Date().toLocaleTimeString();
+      console.log(time);
+      return { time };
+    },
+
+    // ** Initial
+    initialData() {
+      const time = new Date().toLocaleTimeString();
+      console.log(time);
+      return { time };
+    },
+    initialDataUpdatedAt() {
+      return Date.now();
+    },
+
+    // ** Refetch
+    refetchInterval: 1000 * 60 * 30,
+    refetchIntervalInBackground: true,
+
+    // Stale Time
+    staleTime: 1000 * 60 * 30,
+  });
+
   useEffect(() => {
-    if (!data) return;
-    localStorage.setItem("token", data.token);
+    setList((prev) => {
+      const isExist = prev.includes(data.time);
+      if (isExist) return prev;
+      return [...prev, data.time];
+    });
   }, [data]);
 
   return (
     <div className={cx("home b h-100")}>
-      <div></div>
+      <ul> {listEl} </ul>
       <button onClick={handleLogout}>log out</button>
     </div>
   );
