106f343c84009039b7a19e2c8e9ff89e2abef1b2
ZhangSheng <yanglee2421@gmail.com>
2023-08-08 10:19:51 +0800

    Install react hot toast

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,19 @@ import { ThemeProvider } from "@/themes";
 import { ReduxProvider } from "@/redux";
 import { QueryProvider } from "@/api/provider";
 
+// Toast Imports
+import { Toaster } from "react-hot-toast";
+
+// MUI Imports
+import { CssBaseline } from "@mui/material";
+
 export function App() {
   return (
     <ReduxProvider>
       <QueryProvider>
         <ThemeProvider>
+          <Toaster />
+          <CssBaseline />
           <RouterProvider router={router} />
         </ThemeProvider>
       </QueryProvider>
