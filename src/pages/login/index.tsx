f08b75a1fa9650edb2bd97a98eaef29fda858b2c
Yang_Lee <yanglee2421@outlook.com>
2023-08-29 21:46:13 +0800

    IntersectionObserver

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,19 @@
 // Redux Imports
 import { useAppDispatch, sliceLogin } from "@/redux";
+
+// Antd Imports
 import { Button } from "antd";
 
+// Styles Imports
+import styles from "./login.module.scss";
+
+// React Imports
+import { useEffect, useRef } from "react";
+
 export function Component() {
+  // Redux Hooks
   const dispatch = useAppDispatch();
+
   const handleSignIn = (role: "admin" | "client") => {
     const action = sliceLogin.actions.islogged(true);
     const roleAction = sliceLogin.actions.usr({ role });
@@ -11,10 +21,37 @@ export function Component() {
     dispatch(roleAction);
   };
 
+  // IntersectionObserver Hooks
+  const elRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    const el = elRef.current;
+    if (!el) return;
+
+    const observer = new IntersectionObserver(
+      ([{ isIntersecting, intersectionRatio }]) => {
+        if (isIntersecting) {
+          console.log("entry", intersectionRatio);
+          observer.unobserve(el);
+        }
+      }
+    );
+
+    observer.observe(el);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
-      <Button onClick={() => handleSignIn("admin")}>Sign In As Admin</Button>
-      <Button onClick={() => handleSignIn("client")}>Sign In As Client</Button>
+      <section className={styles.box}>
+        <Button onClick={() => handleSignIn("admin")}>Sign In As Admin</Button>
+        <Button onClick={() => handleSignIn("client")}>
+          Sign In As Client
+        </Button>
+      </section>
+      <section ref={elRef}>this</section>
     </>
   );
 }
