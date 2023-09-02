b8a530f4bc69ed20c2aa6cfee53bb2fc23d52b80
Yang_Lee <yanglee2421@outlook.com>
2023-09-03 00:17:36 +0800

    sort out code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,2 @@
-export { DarkSwitch } from "./DarkSwitch";
 export { TinyMCE } from "./tiny-mce";
 export { Reverse } from "./reverse";
-
-// ** HOC
-export { withPortal } from "./with-portal";
-
-export { Counter } from "./Counter";
-export { Collapse } from "./collapse";
