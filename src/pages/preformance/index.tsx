19fbb16dcb8d944b740a229192dbf9214bf048ae
ZhangSheng <yanglee2421@gmail.com>
2023-09-12 15:19:22 +0800

    drag

--- src/pages/preformance/index.tsx
+++ src/pages/preformance/index.tsx
@@ -1,7 +1,53 @@
 // Classes Imports
-import style from "./style.module.scss";
 import clsx from "clsx";
+import React, { useRef } from "react";
 
 export function Component() {
-  return <div className={clsx(["h-full", style.box])}>performance</div>;
+  const ulRef = useRef<HTMLUListElement>(null);
+  const handleDrag: React.DragEventHandler = (evt) => {
+    void evt;
+  };
+
+  return (
+    <div className={clsx("h-full border border-red-400 border-solid")}>
+      <h1>performance</h1>
+      <ul
+        ref={ulRef}
+        onDrag={handleDrag}
+        onDragOver={(e) => e.preventDefault()}
+        className="list-none p-0 w-fit mx-auto "
+      >
+        <li
+          draggable
+          className="border border-solid border-slate-500 w-96 h-16 bg-green-300 mb-2 last:mb-0"
+        >
+          123
+        </li>
+        <li
+          draggable
+          className="border border-solid border-slate-500 w-96 h-16 bg-green-300 mb-2 last:mb-0"
+        >
+          123
+        </li>
+        <li
+          draggable
+          className="border border-solid border-slate-500 w-96 h-16 bg-green-300 mb-2 last:mb-0"
+        >
+          123
+        </li>
+        <li
+          draggable
+          className="border border-solid border-slate-500 w-96 h-16 bg-green-300 mb-2 last:mb-0"
+        >
+          123
+        </li>
+        <li
+          draggable
+          className="border border-solid border-slate-500 w-96 h-16 bg-green-300 mb-2 last:mb-0"
+        >
+          123
+        </li>
+      </ul>
+    </div>
+  );
 }
