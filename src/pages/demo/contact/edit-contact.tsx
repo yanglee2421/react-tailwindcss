cc02be03749c30111a276a8eed61d4d0d4b7a811
ZhangSheng <yanglee2421@gmail.com>
2023-11-07 10:43:36 +0800

    contact demo

---
+++ src/pages/demo/contact/edit-contact.tsx
@@ -0,0 +1,64 @@
+// React Imports
+import React from "react";
+
+export function EditContact({ savedContact, onSave }: EditContactProps) {
+  const [name, setName] = React.useState(savedContact.name);
+  const [email, setEmail] = React.useState(savedContact.email);
+
+  React.useEffect(() => {
+    setName(savedContact.name);
+    setEmail(savedContact.email);
+  }, [savedContact]);
+
+  return (
+    <section>
+      <label>
+        姓名：{" "}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        邮箱：{" "}
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <button
+        onClick={() => {
+          const updatedData = {
+            id: savedContact.id,
+            name: name,
+            email: email,
+          };
+          onSave(updatedData);
+        }}
+      >
+        保存
+      </button>
+      <button
+        onClick={() => {
+          setName(savedContact.name);
+          setEmail(savedContact.email);
+        }}
+      >
+        重置
+      </button>
+    </section>
+  );
+}
+
+export interface EditContactProps {
+  onSave(savedContact: SavedContact): void;
+  savedContact: SavedContact;
+}
+
+interface SavedContact {
+  id: number;
+  name: string;
+  email: string;
+}
--- src/pages/demo/demo.tsx
+++ src/pages/demo/demo.tsx
@@ -1,6 +1,7 @@
 // Components Imports
 import { TodoList } from "./todo-list";
 import { TodoList2 } from "./todo-list2";
+import { Contact } from "./contact";
 
 // Antd Imports
 import { Row, Col } from "antd";
@@ -14,6 +15,9 @@ export function Demo() {
         </Col>
         <Col xs={24} sm={12}>
           <TodoList2 />
+        </Col>
+        <Col xs={24} sm={12}>
+          <Contact />
         </Col>
       </Row>
     </>
