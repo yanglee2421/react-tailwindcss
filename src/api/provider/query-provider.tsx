d31e64ad7dcaaac75feacc3fd47a18a74075e9cc
ZhangSheng <yanglee2421@gmail.com>
2023-11-28 10:01:53 +0800

    Update query provider

--- src/api/provider/query-provider.tsx
+++ src/api/provider/query-provider.tsx
@@ -1,9 +1,64 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./query-client";
+// Query Imports
+import {
+  QueryClient,
+  DefaultOptions,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
 
 export function QueryProvider(props: React.PropsWithChildren) {
+  // ** Props
   const { children } = props;
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: queries(),
+    mutations: mutations(),
+  },
+});
+
+// Persist client
+const persister = createSyncStoragePersister({
+  storage: sessionStorage,
+  key: import.meta.env.VITE_QUERY_PERSISTER_KEY,
+});
+persistQueryClient({
+  queryClient,
+  persister,
+});
+
+// Client configuration
+function queries(): DefaultOptions["queries"] {
+  return {
+    staleTime: 1000 * 60,
+    gcTime: 1000 * 60 * 2,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+    retryDelay(attemptIndex) {
+      return Math.min(1000 * 2 ** attemptIndex, 1000 * 8);
+    },
+  };
+}
+
+function mutations(): DefaultOptions["mutations"] {
+  return {};
+}
+
+// ** Defaults
+queryClient.setQueryDefaults(["unique"], {
+  async queryFn() {
+    return { msg: "hello world" };
+  },
+});
+queryClient.setMutationDefaults(["post-demo"], {
+  async mutationFn() {
+    return { msg: "successly" };
+  },
+});
