80316059f2226b06f426374f376ca5e223adf162
yanglee2421 <yanglee2421@gmail.com>
2023-12-08 00:20:22 +0800

    Sort out code

--- src/main.tsx
+++ src/main.tsx
@@ -16,18 +16,22 @@ import "@/i18n";
 // Fake Database Imports
 import "@/api/fakedb";
 
-// HTML Element
-const el = document.querySelector("#root");
-bootstrap(el);
+init();
 
-function bootstrap(el: Element | null) {
-  if (!el) {
-    console.error("Invalid Element");
+/**
+ * @description Initialize React Application
+ */
+function init() {
+  const container = document.getElementById("root");
+
+  // No Container Element
+  if (!container) {
+    console.error("Can not find element #root");
     return;
   }
 
-  // React Root
-  ReactDOM.createRoot(el).render(
+  // Has Container Element
+  ReactDOM.createRoot(container).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
