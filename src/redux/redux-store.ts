ff6bba96f162a60fe5b03a62fdd13044c4b63701
ZhangSheng <yanglee2421@gmail.com>
2023-12-07 12:01:33 +0800

    Redux configuration

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,5 +1,6 @@
 // Redux Toolkit Imports
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 // Persist Imports
 import {
@@ -19,42 +20,44 @@ import session from "redux-persist/lib/storage/session";
 import { sliceLoginLocal } from "./slice-login-local";
 import { sliceLoginSession } from "./slice-login-session";
 import { sliceTheme } from "./slice-theme";
-
-// Create Reducer
-const rootReducer = combineReducers({
-  [sliceTheme.name]: sliceTheme.reducer,
-  [sliceLoginLocal.name]: sliceLoginLocal.reducer,
-  [sliceLoginSession.name]: persistReducer(
-    {
-      key: sliceLoginSession.name,
-      storage: session,
-      blacklist: [],
-    },
-    sliceLoginSession.reducer
-  ),
-});
-
-// Create Persisted Reducer
-const reducer = persistReducer(
-  {
-    key: import.meta.env.VITE_REDUX_PERSISTER_KEY,
-    storage: local,
-    blacklist: [sliceLoginSession.name],
-  },
-  rootReducer
-);
+import { sliceApi } from "./slice-api";
 
 // Create Store
 export const store = configureStore({
-  reducer,
+  reducer: persistReducer(
+    // Persist configuration
+    {
+      key: import.meta.env.VITE_REDUX_PERSISTER_KEY,
+      storage: local,
+      blacklist: [sliceLoginSession.name, sliceApi.reducerPath],
+    },
+
+    // Root reducer
+    combineReducers({
+      [sliceLoginLocal.name]: sliceLoginLocal.reducer,
+      [sliceLoginSession.name]: persistReducer(
+        {
+          key: sliceLoginSession.name,
+          storage: session,
+          blacklist: [],
+        },
+        sliceLoginSession.reducer
+      ),
+      [sliceTheme.name]: sliceTheme.reducer,
+      [sliceApi.reducerPath]: sliceApi.reducer,
+    })
+  ),
+
+  // ** Middleware
   middleware(getMiddleWare) {
     return getMiddleWare({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    });
+    }).concat(sliceApi.middleware);
   },
 });
+setupListeners(store.dispatch);
 
 // Create Persisted Store
 export const persistor = persistStore(store);
