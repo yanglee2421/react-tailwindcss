debae3aee3227ff0e5829149398d4349a0c0cbd0
ZhangSheng <yanglee2421@gmail.com>
2023-12-09 14:14:59 +0800

    Sort out code

--- src/main.tsx
+++ src/main.tsx
@@ -18,19 +18,14 @@ import "@/api/fakedb";
 
 init();
 
-/**
- * @description Initialize React Application
- */
 function init() {
   const container = document.getElementById("root");
 
-  // No Container Element
   if (!container) {
     console.error("Can not find element #root");
     return;
   }
 
-  // Has Container Element
   ReactDOM.createRoot(container).render(
     <React.StrictMode>
       <App />
