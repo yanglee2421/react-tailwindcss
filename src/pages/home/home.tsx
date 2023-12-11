1afe94758afc086251ea741b4678acdf46f7c00c
ZhangSheng <yanglee2421@gmail.com>
2023-12-11 10:33:58 +0800

    feat: update home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,55 +1,50 @@
-// React Imports
-// import React from "react";
-
 // Antd Imports
 import { Button } from "antd";
 
 // Hooks Imports
-import { useLogin } from "@/hooks";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useLogin, useObserverResize } from "@/hooks";
 
 // React Imports
 import React from "react";
 
+// Clsx Imports
+import clsx from "clsx";
+
 export function Home() {
   const login = useLogin();
 
-  const ref = React.useRef(false);
-
-  const queryClient = useQueryClient();
-  const query = useQuery({
-    queryKey: ["lab query"],
-    async queryFn() {
-      await new Promise((res) => {
-        setTimeout(res, 1000 * 5);
-      });
-
-      if (ref.current) {
-        throw new Error("Error query");
-      }
-      ref.current = true;
-      return { age: 18 };
-    },
-
-    retry: false,
-    // staleTime: 0,
-  });
-  console.log("pengding", query.isPending, query.isLoading, query.isFetching);
-  console.log(
-    "data",
-    query.data,
-    query.isSuccess,
-    queryClient.getQueryData(["lab query"])
-  );
-  console.log("error", query.error, query.isError);
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const entry = useObserverResize(containerRef);
 
   return (
     <>
-      <div>
-        <Button onClick={login.signOut} danger type="primary">
-          logout
-        </Button>
-        <Button onClick={() => query.refetch()}>refetch</Button>
+      <div
+        className={clsx([
+          "h-full flex flex-col",
+          "border border-solid border-red-100",
+          "p-3",
+        ])}
+      >
+        <div>
+          <Button onClick={login.signOut} danger type="primary">
+            logout
+          </Button>
+        </div>
+        <div
+          ref={containerRef}
+          className={clsx([
+            "flex-1 overflow-hidden",
+            "mt-4",
+            "border border-blue-500 border-solid",
+          ])}
+        >
+          <iframe
+            src="http://localhost:3000"
+            width={entry?.contentBoxSize?.[0].inlineSize}
+            height={entry?.contentBoxSize?.[0].blockSize}
+            frameBorder={0}
+          ></iframe>
+        </div>
       </div>
     </>
   );
