f04cdeb279f13ef2548690502fa2cf24b28a12d9
yanglee2421 <yanglee2421@gmail.com>
2026-09-14 23:42:37 +0800

    refactor: Refactor scanner with rxjs

--- apps/cli/src/scanner.ts
+++ apps/cli/src/scanner.ts
@@ -1,70 +1,129 @@
 import net from "node:net";
+import {
+  catchError,
+  concatMap,
+  from,
+  map,
+  Observable,
+  of,
+  Subject,
+  switchMap,
+  take,
+  tap,
+} from "rxjs";
+import { fromFetch } from "rxjs/fetch";
 import { InterByteTimeoutParser, SerialPort } from "serialport";
 
 export const scanBySerialPort = () => {
-  const port = new SerialPort({
-    path: "COM1",
-    baudRate: 9600,
-    dataBits: 8,
-    stopBits: 1,
-    parity: "none",
+  return new Observable<string>((sub) => {
+    const port = new SerialPort({
+      path: "COM1",
+      baudRate: 9600,
+      dataBits: 8,
+      stopBits: 1,
+      parity: "none",
+    });
+
+    // const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+    const parser = port.pipe(new InterByteTimeoutParser({ interval: 30 }));
+
+    parser.on("data", (data: Buffer) => {
+      sub.next(data.toString());
+    });
+    parser.on("error", (error) => {
+      sub.error(error);
+    });
+    parser.on("close", () => {
+      sub.complete();
+    });
+
+    return () => {
+      parser.destroy();
+      port.close();
+    };
   });
-
-  // const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
-  const parser = port.pipe(new InterByteTimeoutParser({ interval: 30 }));
-
-  parser.on("data", (data: Buffer) => {
-    console.log("Data:", data.toString());
-  });
-
-  console.log("Scanner ready:");
 };
 
 export const scanAsTCPClient = () => {
-  const client = net.createConnection(
-    {
+  return new Observable<string>((sub) => {
+    const client = net.createConnection({
       host: "192.168.2.199",
       port: 9601,
-    },
-    () => {
-      console.log("已连接服务器");
-    },
-  );
+    });
 
-  client.on("data", (data) => {
-    console.log("收到服务端:", data.toString());
-  });
+    client.on("data", (data) => {
+      sub.next(data.toString());
+    });
+    client.on("error", (error) => {
+      sub.error(error);
+    });
+    client.on("close", () => {
+      sub.complete();
+    });
 
-  client.on("close", () => {
-    console.log("连接关闭");
-  });
-
-  client.on("error", (error) => {
-    console.error(error);
+    return () => {
+      client.destroy();
+    };
   });
 };
 
 export const scanAsTCPServer = () => {
-  const server = net.createServer((socket) => {
-    console.log("客户端已连接");
-
-    // 收到数据
-    socket.on("data", (data) => {
-      console.log("收到:", data.toString());
+  const socket$ = new Subject<string>();
+  const server$ = new Observable<net.Server>((sub) => {
+    const server = net.createServer((socket) => {
+      socket.on("data", (data) => {
+        socket$.next(data.toString());
+      });
+      socket.on("error", (err) => {
+        socket$.error(err);
+      });
+      socket.on("close", () => {
+        socket$.complete();
+      });
     });
 
-    // 连接关闭
-    socket.on("close", () => {
-      console.log("客户端断开");
+    server.on("listening", () => {
+      sub.next(server);
+    });
+    server.on("error", (error) => {
+      sub.error(error);
+    });
+    server.on("close", () => {
+      sub.complete();
     });
 
-    // 错误处理
-    socket.on("error", (err) => {
-      console.error("socket error:", err);
+    server.listen(9606, "0.0.0.0", () => {
+      console.log("TCP Server listening on 9606");
     });
+
+    return () => {
+      server.close();
+    };
   });
 
-  server.listen(9606, "0.0.0.0", () => {
-    console.log("TCP Server listening on 9606");
-  });
+  return { server$, socket$ };
 };
+
+const windowReady$ = new Subject();
+const inputReady = (data: unknown) => {
+  return windowReady$.pipe(
+    map(() => data),
+    take(1),
+  );
+};
+export const app$ = scanBySerialPort().pipe(
+  concatMap((code) => {
+    const url = new URL("http://localhost:5003");
+
+    url.searchParams.set("param", code);
+
+    return fromFetch(url.href).pipe(
+      switchMap((r) => from(r.json())),
+      catchError(() => of(null)),
+      switchMap((data) => inputReady(data)),
+      tap((data) => {
+        console.log(data);
+      }),
+    );
+  }),
+);
